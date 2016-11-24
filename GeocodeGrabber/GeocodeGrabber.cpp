e35b5c1f24221723bb31633b6c3b981e808ca21a
Nixes <callum@see-bry.com>
2016-11-24 16:57:04 +1100

    almost finished sunrise/sunset time calculation

--- GeocodeGrabber/GeocodeGrabber.cpp
+++ GeocodeGrabber/GeocodeGrabber.cpp
@@ -124,6 +124,8 @@ private:
 			.wait();
 	}
 
+	// What follows is a c++ implementation of the algorithm specified here: http://williams.best.vwh.net/sunrise_sunset_algorithm.htm
+
 	// TODO: Improve variable names
 	int GetDayOfYear() {
 		time_t c_now = time(0);
@@ -137,6 +139,67 @@ private:
 		return a - (b * c) + now.tm_mday - 30;
 	}
 
+	float GetSunriseLongHour(int day_of_year) {
+		float long_hour = longitude / 15;
+
+		float rising_time = day_of_year + ((6 - long_hour) / 24);
+		return rising_time;
+	}
+
+	float GetSunsetLongHour(int day_of_year) {
+		float long_hour = longitude / 15;
+
+		float setting_time = day_of_year + ((18 - long_hour) / 24);
+		return setting_time;
+	}
+
+	float GetSunMeanAnomaly(float time) {
+		return (0.9856 * time) - 3.289;
+	}
+
+	// this is where most of the magic happens: returns sunrise time if true, sunset if false.
+	float GetLocalHours(bool calc_sunrise) {
+		const int day_of_year = GetDayOfYear();
+
+		float t = 0;
+		if (calc_sunrise) {
+			t = GetSunriseLongHour(day_of_year);
+		} else {
+			t = GetSunsetLongHour(day_of_year);
+		}
+
+		const float zenith = 90.83333; // official zenith
+
+		float sun_mean_anomaly = GetSunMeanAnomaly(t);
+
+		float sun_true_longitude = sun_mean_anomaly + (1.916 * sin(sun_mean_anomaly)) + (0.020 * sin(2 * sun_mean_anomaly)) + 282.634;
+
+		float sun_right_ascension = atan(0.91764 * tan(sun_true_longitude));
+
+		float true_long_quadrant = (floor(sun_true_longitude / 90)) * 90;
+		float right_ascension_quadrant = (floor(sun_right_ascension / 90)) * 90;
+		sun_right_ascension = sun_right_ascension + (true_long_quadrant - right_ascension_quadrant);
+
+		// convert right ascension to hours
+		float right_ascension_hours = sun_right_ascension / 15;
+
+		// calc sun declination
+		float sine_declination = 0.39782 * sin(sun_true_longitude);
+		float cosine_declination = cos(asin(sine_declination));
+
+		// calc sun local hour angle
+		float cos_hour_angle = (cos(zenith) - (sine_declination * sin(latitude))) / (cosine_declination * cos(latitude));
+
+		// calculate hours
+		float tmp_hours = 0;
+		if (calc_sunrise) {
+			tmp_hours = 360 - acos(cos_hour_angle);
+		} else {
+			tmp_hours = acos(cos_hour_angle);
+		}
+		float hours = tmp_hours / 15;
+
+	}
 public:
 	GeocodeGrabber(std::string tmp_geocoding_api_key, std::string tmp_geolocation_api_key) {
 		geocoding_api_key = tmp_geocoding_api_key;
@@ -148,11 +211,12 @@ public:
 
 	// return the predicted sunset time as decimal hours
 	float GetSunset() {
+		return GetLocalHours(false);
 	}
 
 	// return the predicted sunrise time as decimal hours
 	float GetSunrise() {
-
+		return GetLocalHours(true);
 	}
 
 	void PrintPrivate() {
@@ -168,6 +232,9 @@ public:
 	void TestIp() {
 		GetLongLatFromIp();
 		std::cout << "Day of year: " << GetDayOfYear() << std::endl;
+
+		std::cout << "SunriseLongHour: " << GetSunriseLongHour() << std::endl;
+		std::cout << "SunsetLongHour: " << GetSunsetLongHour() << std::endl;
 	}
 
 };
