a0ca423ee0631f014b46f3b8bdd975b41348ec54
Nixes <callum@see-bry.com>
2016-12-14 20:38:48 +1100

    break out example file and rename library to header

--- GeocodeGrabber/GeocodeGrabber.hpp
+++ GeocodeGrabber/GeocodeGrabber.hpp
@@ -347,22 +347,3 @@ public:
 		return GetSunriseSunsetTime(true);
 	}
 };
-
-int main() {
-	GeocodeGrabber geocode_test = GeocodeGrabber("AIzaSyD - NPqot8WGQyK0GtcrkMasHPIzKHB - HTo", "AIzaSyBF70jGFpFNUGJFMUOqVLQfTikvKRrdc0U");
-
-	// to specify a location based on an address use
-	std::string address = "John St, Hawthorn VIC";
-	geocode_test.GetLongLatFromAddress(address);
-
-	// to guestimate the users location based on their ip use
-	//geocode_test.GetLongLatFromIp();
-	
-	// get the resulting Sunrise and Sunset times as doubles
-	std::cout << "Sunrise: " << geocode_test.GetSunrise() << std::endl;
-	std::cout << std::endl;
-	std::cout << "Sunset: " << geocode_test.GetSunset() << std::endl;
-
-	system("pause");
-	return 0;
-}
